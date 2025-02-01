d65eb3a52cc44a7749aeb0435e1225fd13da6a7e
Anjali Roy <126140382+anjaliii255@users.noreply.github.com>
2025-02-01 12:30:58 +0530

    Update euler.py

--- sympy/calculus/euler.py
+++ sympy/calculus/euler.py
@@ -10,7 +10,7 @@ from sympy.core.symbol import Symbol
 from sympy.core.sympify import sympify
 from sympy.utilities.iterables import iterable
 
-
+#comment
 def euler_equations(L, funcs=(), vars=()):
     r"""
     Find the Euler-Lagrange equations [1]_ for a given Lagrangian.
